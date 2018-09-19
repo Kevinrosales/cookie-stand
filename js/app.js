e42a13b16ccf29155bb6364a455f177ab78cf0e8
Kevinrosales <kevinrosalesolvera@gmail.com>
2018-09-18 17:04:41 -0700

    made my code much smaller with constructions

--- js/app.js
+++ js/app.js
@@ -2,104 +2,31 @@
 
 var hours = ['6am: ','7am: ','8am: ','9am: ','10am: ','11am: ','12am: ','1pm: ','2pm: ','3pm: ','4pm: ','5pm: ','6pm: ','7pm: ','8pm:'];
 
-var firstAndPike = {
-    min: 23,
-    max: 65,
-    avg: 6.3,
-    cookieSales: [],
-
-    //this is what generates the random rumber between 23 and 65
-    randoMen: function (min, max) {
-        min = Math.ceil(this.min);
-        max = Math.floor(this.max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-      },
-      
-    //list all the hours then multiply the random number with 6.3 and rounds it
-    makeAvg: function () {
-      for(var i = 0; i < hours.length; i ++){
-        var cookieSale = this.randoMen() * this.avg;
-        console.log (Math.round(cookieSale));
-        }
-    }
+var Store = function(name, min, max, avg) {
+    this.storeName = name;
+    this.minCust = min;
+    this.maxCust = max;
+    this.avgSales = avg;
+    this.cookieSales = [];
 };
 
-var seaTacAirport = {
-    min: 3,
-    max: 11,
-    avg: 1.2,
-    cookieSales: [],
+ Store.prototype.ranNum = function(min, max) {
+    min = this.minCust;
+    max = this.maxCust;
+    var randomHourlyCust = Math.round(Math.random() * (max - min) + min);
+    // console.log(randomHourlyCust);
+    return randomHourlyCust;
 
-    randoMen: function (min, max) {
-        min = Math.ceil(this.min);
-        max = Math.floor(this.max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-      },
-      
-    makeAvg: function () {
-      for(var i = 0; i < hours.length; i ++){
-        var cookieSale = this.randoMen() * this.avg;
-        console.log (Math.round(cookieSale));
-        }
+    };
+
+Store.prototype.findSales = function() {
+    for (var i =0;i < hours.length; i++) {
+      this.cookieSales.push(Math.round(this.ranNum() * this.avgSales));   
     }
-};
-
-var seattleCenter = {
-    min: 11,
-    max: 38,
-    avg: 3.7,
-    cookieSales: [],
-
-    randoMen: function (min, max) {
-        min = Math.ceil(this.min);
-        max = Math.floor(this.max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-      },
-      
-    makeAvg: function () {
-      for(var i = 0; i < hours.length; i ++){
-        var cookieSale = this.randoMen() * this.avg;
-        console.log (Math.round(cookieSale));
-        }
-    }
-};
-
-var capitolHill = {
-    min: 20,
-    max: 38,
-    avg: 2.3,
-    cookieSales: [],
-
-    randoMen: function (min, max) {
-        min = Math.ceil(this.min);
-        max = Math.floor(this.max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-      },
-      
-    makeAvg: function () {
-      for(var i = 0; i < hours.length; i ++){
-        var cookieSale = this.randoMen() * this.avg;
-        console.log (Math.round(cookieSale));
-        }
-    }
-};
-
-var alki = {
-    min: 2,
-    max: 16,
-    avg: 4.6,
-    cookieSales: [],
-
-    randoMen: function (min, max) {
-        min = Math.ceil(this.min);
-        max = Math.floor(this.max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-      },
-      
-    makeAvg: function () {
-      for(var i = 0; i < hours.length; i ++){
-        var cookieSale = this.randoMen() * this.avg;
-        console.log (Math.round(cookieSale));
-        }
-    }
-};
+}
+    
+var pike = new Store('First and Pike', 23, 65, 6.3);  
+var seaTac = new Store('Seatac AirPort', 3, 11, 1.2);
+var seattle = new Store('Seattle Center', 11, 38, 3.7);
+var capitol = new Store('Capitol Hill', 20, 38, 2.3);
+var alki = new Store('Alki', 2, 16, 4.6);''
