348350d58660a952d068f2cb2ad208b6a7585045
Kevinrosales <kevinrosalesolvera@gmail.com>
2018-09-18 23:00:49 -0700

    tried to get the Dom working but did not get it

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 'use strict'
 
 var hours = ['6am: ','7am: ','8am: ','9am: ','10am: ','11am: ','12am: ','1pm: ','2pm: ','3pm: ','4pm: ','5pm: ','6pm: ','7pm: ','8pm:'];
+var newLi;
+var displaySales;
+var postiton;
+var i;
 
 var Store = function(name, min, max, avg) {
     this.storeName = name;
@@ -30,3 +34,11 @@ var seaTac = new Store('Seatac AirPort', 3, 11, 1.2);
 var seattle = new Store('Seattle Center', 11, 38, 3.7);
 var capitol = new Store('Capitol Hill', 20, 38, 2.3);
 var alki = new Store('Alki', 2, 16, 4.6);''
+
+for(i =0; i <= hours.length; i++) {
+    newLi = document.createElement('li');
+    displaySales = document.createTextNode(pike.cookieSales[i]);
+    newLi.appendChild(displaySales);
+    postiton = document.getElementById('pike');
+    postiton.appendChild(newLi);
+}
