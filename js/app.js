eabba52e99477417828cf114b611d3255562684d
Kevinrosales <kevinrosalesolvera@gmail.com>
2018-09-19 23:09:48 -0700

    started to really get my graph going

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 'use strict'
 
 var hours = ['6am: ','7am: ','8am: ','9am: ','10am: ','11am: ','12am: ','1pm: ','2pm: ','3pm: ','4pm: ','5pm: ','6pm: ','7pm: ','8pm:'];
-var newLi;
-var postiton = document.getElementById('table');;
+var table = document.getElementById('table');
 var i;
+var newLi;
+var postiton;
+var allStores = [];
+var placeTable = document.getElementById('table');
 
 var Store = function(name, min, max, avg) {
     this.storeName = name;
@@ -11,6 +14,8 @@ var Store = function(name, min, max, avg) {
     this.maxCust = max;
     this.avgSales = avg;
     this.cookieSales = [];
+    this.findSales();
+    allStores.push(this);
 };
 
  Store.prototype.ranNum = function(min, max) {
@@ -22,24 +27,64 @@ var Store = function(name, min, max, avg) {
 };
 
 Store.prototype.findSales = function() {
-    for (var i =0;i < hours.length; i++) {
+    for (i =0;i < hours.length; i++) {
         this.cookieSales.push(Math.round(this.ranNum() * this.avgSales));   
     }
 }
 
-var pike = new Store('First and Pike', 23, 65, 6.3);  
-var seaTac = new Store('Seatac AirPort', 3, 24, 1.2);
-var seattle = new Store('Seattle Center', 11, 38, 3.7);
-var capitol = new Store('Capitol Hill', 20, 38, 2.3);
-var alki = new Store('Alki', 2, 16, 4.6);
-// console.log(pike);
+Store.prototype.render = function() {
+    var trElement = document.createElement('tr');
+    var tdNameElement = document.createElement('th');
+    tdNameElement.textContent = this.name;
+    trElement.appendChild(tdNameElement);
 
-pike.findSales();
-for(i =0; i < 1; i++) { 
-    newLi = document.createElement('li');
-    newLi.textContent = pike.cookieSales;
-    postiton.appendChild(newLi);
-    console.log(pike.cookieSales);
-    //pike.findSales() first then 
-    //pike.cookieSales gets the data in the console.
+    for(i=0; i < hours.length; i++) {
+        var tdElement = document.createElement('td');
+        tdElement.textContent = this.cookieSales[i];
+        trElement.appendChild(tdElement);
+    }
+        placeTable.appendChild(trElement);    
 }
+
+function makeHeaderRow() {
+    var tableHeaderElement = document.createElement('tHead');
+    var trTableHeaderElement = document.createElement('tr');
+    trTableHeaderElement.appendChild(document.createElement('th'));
+
+    for(i=0; i < hours.length; i++){
+        var tdHeaderElement = document.createElement('th');
+        tdHeaderElement.appendChild(document.createTextNode(hours[i]));
+        trTableHeaderElement.appendChild(tdHeaderElement);
+    }
+    table.appendChild(trTableHeaderElement);
+}
+
+new Store('First and Pike', 23, 65, 6.3);  
+new Store('Seatac AirPort', 3, 24, 1.2);
+new Store('Seattle Center', 11, 38, 3.7);
+new Store('Capitol Hill', 20, 38, 2.3);
+new Store('Alki', 2, 16, 4.6);
+
+
+
+
+// for(i =0; i < 1; i++) { 
+//  newLi = document.createElement('li');
+//  newLi.textContent = this.cookieSales;
+//  postiton.appendChild(newLi);
+//  console.log(this.cookieSales);
+
+//  pike.findSales() first then 
+//  pike.cookieSales gets the data in the console.
+//  }
+
+// findSales.prototype.render = function() {
+//     var trElement = document.createElement('tr');
+//     var tdElement = document.createElement('td');
+//     tdElement.textContent = this.Store;
+//     trElement.appendChild(tdElement);
+//     tdElement = document.createElement('td');
+//     tdElement.textContent = this.;
+
+
+// }
